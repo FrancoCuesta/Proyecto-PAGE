6352b3d00677dea58d656c4bb8481926e229fa3f
Lolellus <81826356+Lolellus@users.noreply.github.com>
2022-08-28 10:34:23 -0300

    form

--- Chatbot-UI/chatbot-ui.js
+++ Chatbot-UI/chatbot-ui.js
@@ -1,23 +1,15 @@
-
 function init() {
-
     //---------------------------- Including Jquery ------------------------------
-
     var script = document.createElement('script');
     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
-
     //--------------------------- Variables Importantes ----------------------------
     botLogoPath = "./imgs/bot-logo.png"
-
     //--------------------------- Chatbot Frontend -------------------------------
     const chatContainer = document.getElementById("chat-container");
-
     template = ` <button class='chat-btn'><img src = "./icons/comment.png" class = "icon" ></button>
-
     <div class='chat-popup'>
-    
 		<div class='chat-header'>
 			<div class='chatbot-img'>
 				<img src='${botLogoPath}' alt='Chat Bot image' class='bot-img'> 
@@ -25,46 +17,34 @@ function init() {
 			<h3 class='bot-title'>Covid Bot</h3>
 			<button class = "expand-chat-window" ><img src="./icons/open_fullscreen.png" class="icon" ></button>
 		</div>
-
 		<div class='chat-area'>
             <div class='bot-msg'>
                 <img class='bot-img' src ='${botLogoPath}' />
 				<span class='msg'>Hi, How can i help you?</span>
 			</div>
-
             <!-- <div class='bot-msg'>
                 <img class='bot-img' src ='${botLogoPath}' />
                 <div class='response-btns'>
                     <button class='btn-primary' onclick= 'userResponseBtn(this)' value='/sign_in'>sample btn</button>            
                 </div>
 			</div> -->
-
 			<!-- <div class='bot-msg'>
 				<img class='msg-image' src = "https://i.imgur.com/nGF1K8f.jpg" />
 			</div> -->
-
 			<!-- <div class='user-msg'>
 				<span class='msg'>Hi, How can i help you?</span>
 			</div> -->
-			
-
 		</div>
-
-
 		<div class='chat-input-area'>
 			<input type='text' autofocus class='chat-input' onkeypress='return givenUserInput(event)' placeholder='Type a message ...' autocomplete='off'>
 			<button class='chat-submit'><i class='material-icons'>send</i></button>
 		</div>
-
 	</div>`
-
 
     chatContainer.innerHTML = template;
 
     //--------------------------- Variables Importantes----------------------------
     var inactiveMessage = "El servidor está inactivo, comuníquese con el desarrollador para activarlo"
-
-
     chatPopup = document.querySelector(".chat-popup")
     chatBtn = document.querySelector(".chat-btn")
     chatSubmit = document.querySelector(".chat-submit")
@@ -76,11 +56,8 @@ function init() {
     chatPopup.style.display = "none"
     var host = ""
 
-
     //------------------------ ChatBot Toggler -------------------------
-
     chatBtn.addEventListener("click", () => {
-
         mobileDevice = !detectMob()
         if (chatPopup.style.display == "none" && mobileDevice) {
             chatPopup.style.display = "flex"
@@ -116,16 +93,12 @@ function init() {
             root.style.setProperty('--chat-window-height', 500 + "px");
             root.style.setProperty('--chat-window-total-width', 380 + "px");
         }
-
     })
 }
 
 // fin de la funcion  init
 
-
-
 var passwordInput = false;
-
 function userResponseBtn(e) {
     send(e.value);
 }
@@ -156,8 +129,6 @@ function setUserResponse() {
     }
     scrollToBottomOfResults();
 }
-
-
 
 function scrollToBottomOfResults() {
     chatArea.scrollTop = chatArea.scrollHeight;
@@ -196,20 +167,16 @@ function send(message) {
     chatInput.focus();
 }
 
-
 //------------------------------------ Set bot response -------------------------------------
 function setBotResponse(val) {
     setTimeout(function() {
         if (val.length < 1) {
-            //si no hay respuesta de rasa
-            // msg = 'El servidor está inactivo, comuníquese con el desarrollador para activarlo';
+            //si no hay respuesta de rasa el msg = 'El servidor está inactivo, comuníquese con el administrador para activarlo';
             msg = inactiveMessage;
-
             var BotResponse = `<div class='bot-msg'><img class='bot-img' src ='${botLogoPath}' /><span class='msg'> ${msg} </span></div>`;
             $(BotResponse).appendTo('.chat-area').hide().fadeIn(1000);
             scrollToBottomOfResults();
             chatInput.focus();
-
         } else {
             //if ecibimos respuesta de Rasa
             for (i = 0; i < val.length; i++) {
@@ -226,25 +193,20 @@ function setBotResponse(val) {
 
                 //comprobar si hay imagen
                 if (val[i].hasOwnProperty("image")) {
-                    var BotResponse = `<div class='bot-msg'><img class='bot-img' src ='${botLogoPath}' /><img class='msg-image' src="${val[i].image }"> +</div>`
+                    var BotResponse = `<div class='bot-msg'><img class='bot-img' src ='${botLogoPath}' /><img class='msg-image' src="${val[i].image}"> +</div>`
                     $(BotResponse).appendTo('.chat-area').hide().fadeIn(1000);
                 }
-
                 //comprobar si hay botones
                 if (val[i].hasOwnProperty("buttons")) {
                     var BotResponse = `<div class='bot-msg'><img class='bot-img' src ='${botLogoPath}' /><div class='response-btns'>`
-
                     buttonsArray = val[i].buttons;
                     buttonsArray.forEach(btn => {
                         BotResponse += `<button class='btn-primary' onclick= 'userResponseBtn(this)' value='${btn.payload}'>${btn.title}</button>`
                     })
-
                     BotResponse += "</div></div>"
-
                     $(BotResponse).appendTo('.chat-area').hide().fadeIn(1000);
                     chatInput.disabled = true;
                 }
-
             }
             scrollToBottomOfResults();
             chatInput.disabled = false;
@@ -254,19 +216,14 @@ function setBotResponse(val) {
     }, 500);
 }
 
-
-
-
 function mobileView() {
     $('.chat-popup').width($(window).width());
 
     if (chatPopup.style.display == "none") {
         chatPopup.style.display = "flex"
-            // chatInput.focus();
         chatBtn.style.display = "none"
         chatPopup.style.bottom = "0"
         chatPopup.style.right = "0"
-            // chatPopup.style.transition = "none"
         expandWindow.innerHTML = `<img src = "./icons/close.png" class = "icon" >`
     }
 }
@@ -287,8 +244,6 @@ function chatbotTheme(theme) {
         background: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)"
     }
 
-
-
     if (theme === "orange") {
         root.style.setProperty('--chat-window-color-theme', orange.color);
         gradientHeader.style.backgroundImage = orange.background;
@@ -301,16 +256,13 @@ function chatbotTheme(theme) {
 }
 
 function createChatBot(hostURL, botLogo, title, welcomeMessage, inactiveMsg, theme = "blue") {
-
     host = hostURL;
     botLogoPath = botLogo;
     inactiveMessage = inactiveMsg;
     init()
     const msg = document.querySelector(".msg");
     msg.innerText = welcomeMessage;
-
     const botTitle = document.querySelector(".bot-title");
     botTitle.innerText = title;
-
     chatbotTheme(theme)
 }
--- demo/actions/actions.py
+++ demo/actions/actions.py
@@ -1,27 +1,50 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_name_form"
+
+    def validate_first_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+
+        # Si el nombre es muy corto, podría estar equivocado.
+        name = clean_name(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="Eso debe haber sido un error tipográfico.")
+            return {"first_name": None}
+        return {"first_name": name}
+
+    def validate_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `last_name` value."""
+
+        # Si el nombre es muy corto, podría estar equivocado.
+        name = clean_name(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="Eso debe haber sido un error tipográfico.")
+            return {"last_name": None}
+        
+        first_name = tracker.get_slot("first_name")
+        if len(first_name) + len(name) < 3:
+            dispatcher.utter_message(text="Ese es un nombre muy corto. Tememos un error tipográfico. reiniciando!")
+            return {"first_name": None, "last_name": None}
+        return {"last_name": name}
